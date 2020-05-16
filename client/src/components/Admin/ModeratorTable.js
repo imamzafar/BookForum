1b23feddd55be8000c40b5aee555e552209588a6
poem <normamoras@gmail.com>
2020-05-16 09:52:03 -0400

    update

--- client/src/components/Admin/ModeratorTable.js
+++ client/src/components/Admin/ModeratorTable.js
@@ -11,8 +11,8 @@ function ModeratorTable() {
         // console.log(apiGetUsers);
         // let date = Date(apiGetUsers[1].createdAt).toString().substring(0, 15)
         apiGetUsers.forEach(element => { 
-            element.createdAt = new Date(element.createdAt).toString().substring(0, 15)   
-            element.updateAt = new Date(element.updatedAt).toString().substring(0, 15) 
+            element.createdAt = new Date(element.createdAt).toString().substring(4, 15)   
+            element.updateAt = new Date(element.updatedAt).toString().substring(4, 15) 
         });
         setUsers(apiGetUsers);
         // console.log(date)
@@ -22,11 +22,11 @@ function ModeratorTable() {
     function handlePointsChange(e){
         e.preventDefault();
         setUsers( []);
-        console.log('the empyty array is', users)
+        // console.log('the empyty array is', users)
        let pointsSort = users.sort( function(x, y) {
             return y.points - x.points
         });
-        console.log('sort array is', pointsSort)
+        // console.log('sort array is', pointsSort)
         setUsers([...pointsSort])
     }
     console.log(users);
@@ -35,9 +35,6 @@ function ModeratorTable() {
         e.preventDefault();
 
     }
-
-
-
     useEffect( function(){
         loadPage();
         
@@ -47,34 +44,33 @@ function ModeratorTable() {
         <div class="row">
             <div class="col-lg-12">
             <div class="row justify-content-center mt-2">
-        <div class="col-lg-10 mt-4">
-            <table class="table table-sm">
-                    <thead>
-                        <tr style={{height: "50px", border: "3px solid #9f6934", backgroundColor: "#9f6934", padding: "0", margin: "0", color: "white"}}>
-                            <th  style={{ width:"20%" }}>Name</th>
-                           
-                            <th style={{ width: "20%" }} >
-                                <form class="form-inline">
-                                    <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Member Since</label>
-                                    <button onClick={handleDateChange}><i class="fas fa-caret-down"></i></button>
-                                </form>      
-                            </th>
-                            <th style={{ width: "20%" }} >
-                                <form class="form-inline">
-                                    <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Points</label>
-                                    <button onClick={handlePointsChange}><i class="fas fa-caret-down"></i></button>
-                                </form> 
-                            
-                            </th>
-                            <th style={{ width: "20%" }} >User Type</th>
-                        </tr>
-                    </thead>
-                    <TableDiv users={users} />
-                   
-                    
-            </table>
-        </div>
-    </div>
+                    <div class="col-lg-10 mt-4">
+                        <table class="table table-sm">
+                                <thead>
+                                    <tr style={{height: "50px", border: "3px solid #9f6934", backgroundColor: "#9f6934", padding: "0", margin: "0", color: "white"}}>
+                                        <th  style={{ width:"20%" }}>Name</th>
+                                    
+                                        <th style={{ width: "20%" }} >
+                                            <form class="form-inline">
+                                                <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Member Since</label>
+                                                <button onClick={handleDateChange}><i class="fas fa-caret-down"></i></button>
+                                            </form>      
+                                        </th>
+                                        <th style={{ width: "20%" }} >
+                                            <form class="form-inline">
+                                                <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Points</label>
+                                                <button onClick={handlePointsChange}><i class="fas fa-caret-down"></i></button>
+                                            </form> 
+                                        
+                                        </th>
+                                        <th style={{ width: "20%" }} >User Type</th>
+                                        <th style={{ width: "20%" }} >Action</th>
+                                    </tr>
+                                </thead>
+                                <TableDiv users={users} />
+                        </table>
+                    </div>
+                </div>
               
                
                 
--- client/src/components/Admin/TableDiv.js
+++ client/src/components/Admin/TableDiv.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 
 
 function TableDiv(props) {
-    console.log('the props is', props)
+    // console.log('the props is', props)
     
     return (
 
@@ -15,6 +15,14 @@ function TableDiv(props) {
                     <td>{item.createdAt}</td>
                     <td>{item.points}</td>
                     <td>{item.userType}</td>
+                    <td>
+                    
+                        <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                            <option selected>Choose</option>
+                            <option value="1">Moderator</option>
+                            <option value="2">Delete</option>
+                        </select><button type="submit" style={{border: '1px solid #9f6934'}}class="btn">Submit</button>
+                    </td>
                 </tr> ): '' }
             
         </tbody>
