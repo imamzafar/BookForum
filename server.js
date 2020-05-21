3f8b4fd7df8dd9228b0c026cb3f3561746bb2103
poem <normamoras@gmail.com>
2020-05-21 08:23:59 -0400

    changes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 // var server = app.listen( PORT, function(){ console.log( `[Woof woof], http://localhost:${PORT}` ); });
 app.use( express.static('client/build/') );
+app.use( express.static('./client/build/') );
 // app.use(express.static(path.join(__dirname, "client/src/components/Genre")));
 app.use( express.urlencoded({ extended: false }) );
 app.use( express.json() );
