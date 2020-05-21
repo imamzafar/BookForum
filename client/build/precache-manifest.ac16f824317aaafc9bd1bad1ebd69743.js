a71343d5340fc94661fdbd5ca2dd1ae0472ebcae
poem <normamoras@gmail.com>
2020-05-21 08:32:17 -0400

    add changes

---
+++ client/build/precache-manifest.ac16f824317aaafc9bd1bad1ebd69743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc33bd457ecce15e33b20447624d476",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62c2c092d07e7daf47e9",
+    "url": "/static/css/main.0ee91b04.chunk.css"
+  },
+  {
+    "revision": "c27a4b863c6da5deeae6",
+    "url": "/static/js/2.4f79e84f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4f79e84f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c2c092d07e7daf47e9",
+    "url": "/static/js/main.46ced1d8.chunk.js"
+  },
+  {
+    "revision": "e3450308b316e08a015a",
+    "url": "/static/js/runtime-main.fb31c953.js"
+  }
+]);
