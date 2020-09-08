84cf8d07a25f990674af57dccf4ca529639cb71d
poem <normamoras@gmail.com>
2020-09-08 14:53:58 -0400

    updates mongoddb URI

---
+++ db/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongodb:{
+        DB_URI:'mongodb+srv://db321:db321@cluster0.sgyeu.mongodb.net/book-forum?retryWrites=true&w=majority'
+    }
+}
+
+// //https://www.youtube.com/watch?v=o9e3ex-axzA
+ //https://www.youtube.com/watch?v=ySFXduSdpxs&list=PL4cUxeGkcC9jdm7QX143aMLAqyM-jTZ2x&index=10
