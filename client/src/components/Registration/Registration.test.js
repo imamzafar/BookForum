8b13a3b573a43a96054ad33bfb18a109d37f45b9
poem <normamoras@gmail.com>
2020-09-10 18:26:15 -0400

    to be in the document registration page test

--- client/src/components/Registration/Registration.test.js
+++ client/src/components/Registration/Registration.test.js
@@ -1,3 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Redirect } from "react-router-dom";
 import Registration from './Registration';
+
+describe ('Registration', () => {
+    test('renders Registration component', () => {
+        render ( <Registration />);
+
+        screen.debug();
+
+        expect(screen.getByText(/User Registration/)).toBeInTheDocument();
+    });
+});
